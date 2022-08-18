f5c9c57e196e11ead1b5d8f5602e9051274e4867
shashank-sharma333 <40907278+shashank-sharma333@users.noreply.github.com>
2022-08-18 09:51:54 -0700

    Time: 1 ms, Memory: 41.8 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -1,46 +1,39 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        
-        int maxValue=0;
-        int length=nums.length;
-        
-        int max=nums[0];
-        boolean twice=false;
-        int index=0;
-        for(int counter=0;counter<length;counter++)
-        {
-            if(nums[counter]>max)
-            {
-                max=nums[counter];
-                index=counter;
-            }
-        }
-        
-          for(int counter=0;counter<length;counter++)
-        {
-              if(counter!=index)
-              {
-                  if(max>=2*nums[counter])
-              {
-                  twice=true;
-              }                  
-                  
-            else{
-                      twice=false;
-                        return -1;
-                        }
-         }             
-          }
-        
-        
-       if(twice)
-       {
-           return index;
-       }
-        
-        else{
-            return -1;
-        }
-        
-    }
+
+		int maxValue = 0;
+		int length = nums.length;
+
+		int max = nums[0];
+		boolean twice = false;
+		int index = 0;
+		for (int counter = 0; counter < length; counter++) {
+			if (nums[counter] > max) {
+				max = nums[counter];
+				index = counter;
+			}
+		}
+
+		for (int counter = 0; counter < length; counter++) {
+			if (counter != index) {
+				if (max >= 2 * nums[counter]) {
+					twice = true;
+				}
+
+				else {
+					twice = false;
+					return -1;
+				}
+			}
+		}
+
+		if (twice) {
+			return index;
+		}
+
+		else {
+			return -1;
+		}
+
+	}
 }
