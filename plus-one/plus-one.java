eaf12d09fcc99d60949db5c3bfce4aa277814a12
shashank-sharma333 <40907278+shashank-sharma333@users.noreply.github.com>
2022-08-18 09:27:30 -0700

    Time: 1 ms, Memory: 42.9 MB - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-    int n = digits.length;
-    for(int i=n-1; i>=0; i--) {
-        if(digits[i] < 9) {
-            digits[i]++; return digits;
+    int length= digits.length;
+    for(int counter=length-1; counter>=0; counter--) {
+        if(digits[counter] < 9) {
+            digits[counter]++; return digits;
         }
-        digits[i] = 0;
+        digits[counter] = 0;
     }
     
-    int[] newNumber = new int [n+1];
+    int[] newNumber = new int [length+1];
     newNumber[0] = 1;
     return newNumber;
         
