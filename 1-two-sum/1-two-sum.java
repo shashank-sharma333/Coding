7efab22627b1786e86f861c3cb46fe23fcd38367
shashank-sharma333 <40907278+shashank-sharma333@users.noreply.github.com>
2022-08-28 15:44:47 -0700

    Time: 6 ms (59.69%), Space: 47.8 MB (5.04%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,5 +1,27 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    
+    public int[] twoSum(int[] nums, int target) 
+      {
+        int[] solution = new int[2]; 
+        
+        // HashMap created with corresponding key(data) and value (index)
+        HashMap<Integer,Integer> dataMap = new HashMap();
+        
+        for(int i=0;i<nums.length;i++){
+            if(dataMap.containsKey(target - nums[i])){
+                solution[0] = i;
+                solution[1] = dataMap.get(target - nums[i]);
+            }
+          else{
+                   dataMap.put(nums[i],i);                                                     
+          }
+        }
+     return solution;
+    }
+}
+    /** 
+   O(N2) approach
+   public int[] twoSum(int[] nums, int target) {
         
         int temp=0;
         int a[]=new int[2];
@@ -18,5 +40,4 @@ class Solution {
         
         return a;
         
-    }
-}
+    }**/
