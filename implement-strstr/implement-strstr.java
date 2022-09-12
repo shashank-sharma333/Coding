cde375c27ef35e1607a54ba0c2d3c556e0dc5fc3
shashank-sharma333 <40907278+shashank-sharma333@users.noreply.github.com>
2022-09-11 20:44:51 -0700

    Time: 0 ms, Memory: 42.4 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,6 +1,11 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+       
+        return haystack.indexOf(needle);
         
+        /*
+        
+       Solving using substring way 
         for(int outerCounter=0;outerCounter<haystack.length();outerCounter++)
         {
             for(int innerCounter=outerCounter+1;innerCounter<=haystack.length();innerCounter++)
@@ -14,6 +19,6 @@ class Solution {
         
         }
         
-        return -1;   
+        return -1;   */
     }
 }
